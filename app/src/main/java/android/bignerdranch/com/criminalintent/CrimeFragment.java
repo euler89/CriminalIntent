e8d380f3f92163c32780fcb7a22c98c407c4c8aa
euler89 <rdz_fco@yahoo.com.mx>
2016-05-18 00:05:13 -0500

    New features added: DateDialog.

--- app/src/main/java/android/bignerdranch/com/criminalintent/CrimeFragment.java
+++ app/src/main/java/android/bignerdranch/com/criminalintent/CrimeFragment.java
@@ -1,7 +1,10 @@
 package android.bignerdranch.com.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -12,6 +15,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -24,6 +28,9 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE ="DialogDate";
+
+    private static final int REQUEST_DATE = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -62,8 +69,17 @@ public class CrimeFragment extends Fragment {
 
         //DATE BUTTON
         mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        updateDate();
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment
+                        .newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
         ////CHECK BOX
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
@@ -87,5 +103,23 @@ public class CrimeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }//newInstance()
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+
+        if(requestCode == REQUEST_DATE){
+            Date date = (Date) data.
+                    getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
+    }
 }
 
--- app/src/main/java/android/bignerdranch/com/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/android/bignerdranch/com/criminalintent/SingleFragmentActivity.java
@@ -20,17 +20,6 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         FragmentManager fm = getSupportFragmentManager();
 
